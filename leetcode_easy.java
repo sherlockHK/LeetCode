3af78980dc046f4f7210eb9f51a4a9ee45450aeb
Kai Hu <hukai@sunlands.com>
2019-11-12 16:12:47 +0800

    first: 13

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -24,20 +24,118 @@
 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
 
 示例 1:
-
 输入: 121
 输出: true
-示例 2:
 
+示例 2:
 输入: -121
 输出: false
 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
-示例 3:
 
+示例 3:
 输入: 10
 输出: false
 解释: 从右向左读, 为 01 。因此它不是一个回文数。
 进阶:
-
 你能不将整数转为字符串来解决这个问题吗？
 */
+
+public boolean test(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        List<Integer> list = new ArrayList<>();
+        while (x != 0){
+            int re = x % 10;
+            x = x / 10;
+            list.add(re);
+        }
+
+        boolean flag = true;
+        int size = list.size();
+        if (size >1){
+            for(int i = 0; i < size/2; i++){
+                if (!list.get(i).equals(list.get(size - i -1))){
+                    flag = false;
+                }
+            }
+        }
+        return flag;
+    }
+
+/*
+13.罗马数字转整数
+
+罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，
+例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。
+同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+
+示例 1:
+输入: "III"
+输出: 3
+
+示例 2:
+输入: "IV"
+输出: 4
+
+示例 3:
+输入: "IX"
+输出: 9
+
+示例 4:
+输入: "LVIII"
+输出: 58
+解释: L = 50, V= 5, III = 3.
+
+示例 5:
+输入: "MCMXCIV"
+输出: 1994
+解释: M = 1000, CM = 900, XC = 90, IV = 4.
+*/
+
+private int calculateLC(String s) {
+        List<String> list = Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I");
+        Map<String, Integer> map = new HashMap<>();
+        map.put("M", 1000);
+        map.put("CM", 900);
+        map.put("D", 500);
+        map.put("CD", 400);
+        map.put("C", 100);
+        map.put("XC", 90);
+        map.put("L", 50);
+        map.put("XL", 40);
+        map.put("X", 10);
+        map.put("IX", 9);
+        map.put("V", 5);
+        map.put("IV", 4);
+        map.put("I", 1);
+        int result = 0;
+        for (int i = 0; i < list.size(); i++) {
+            String prefix = list.get(i);
+            while (s.startsWith(prefix)){
+                Integer value = map.get(prefix);
+                result += value;
+
+                s = s.substring(prefix.length());
+            }
+        }
+
+        return result;
+    }
+
+    
