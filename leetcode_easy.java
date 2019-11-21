645a74d60f084eb634a675416cff04506d6af6b2
Kai Hu <hukai@sunlands.com>
2019-11-21 19:43:03 +0800

    first:38

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -295,7 +295,7 @@ public int removeElement(int[] nums, int val) {
 }
 
 /**
-28.查找字符串
+28.字符串匹配（KMP）
 实现 strStr() 函数。
 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。
 如果不存在，则返回  -1。
@@ -327,10 +327,52 @@ public int strStr(String haystack, String needle) {
     return -1;
 }
 
+/**
+38.搜索插入位置（二分法）
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+你可以假设数组中无重复元素。
 
+示例 1:
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+输入: [1,3,5,6], 7
+输出: 4
+示例 4:
+输入: [1,3,5,6], 0
+输出: 0
+*/
+//暴力 O(n)
+public int searchInsert(int[] nums, int target){
+for (int i = 0; i < nums.length; i++) {
+        if (target <= nums[i]){
+            return i;
+        }
+    }
+    return nums.length;
+}
 
+//二分法
+public int binarySearch(int[] nums, int target){
+    int l =0;
+    int r = nums.length - 1;
+    while(l <= r){
+        int mid = (l + r) >>> 1;
+        if (nums[mid] == target) {
+            return mid;
+        }
 
-
+        if (nums[mid] > target) {
+            r = mid -1;
+        }else{
+            l = mid + 1;
+        }
+    }
+    return l;
+}
 
 
 
