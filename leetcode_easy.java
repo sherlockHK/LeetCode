b5cc9717f7b2f85b87faa64fe6f5eb3fc985ed6a
Kai Hu <hukai@sunlands.com>
2019-12-03 14:58:01 +0800

    first:58

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -430,7 +430,42 @@ public int maxSubArray(int[] nums) {
     return max;
 }
 
+/**
+58.最后一个单词的长度
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+如果不存在最后一个单词，请返回 0 。
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
 
+示例:
+输入: "Hello World"
+输出: 5
+*/
+//分割字符串
+public int lengthOfLastWord(String s) {
+    String[] strArray = s.split(" ");
+    if (strArray.length == 0) return 0;
+    return strArray[strArray.length - 1].length();
+}
+
+//O(n)
+public int lengthOfLastWord(String s) {
+    if (s == null || s.length() == 0) return 0;
+    int count = 0;
+
+    for (int i = s.length() -1; i >= 0; i--) {
+        if (s.charAt(i) != ' '){
+            count++;
+        }else if (count != 0){
+            break;
+        }
+    }
+
+    return count;
+}
+
+/**
+66.加一
+*/
 
 
 
