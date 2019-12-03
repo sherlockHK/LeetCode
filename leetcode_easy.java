01088b28fb06c3ef730b623336b043f07652ad04
Kai Hu <hukai@sunlands.com>
2019-12-03 16:49:56 +0800

    first:66

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -465,10 +465,62 @@ public int lengthOfLastWord(String s) {
 
 /**
 66.加一
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+示例 1:
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+示例 2:
+输入: [4,3,2,1]
+输出: [4,3,2,2]
+解释: 输入数组表示数字 4321。
 */
+//实现比较丑陋
+public int[] plusOne(int[] digits){
+    int p = 0;
+    for (int i = digits.length -1; i >=0 ; i--) {
+        if (i == digits.length -1){
+            digits[i] += 1;
+        }else {
+            digits[i] += p;
+            if (p == 1){
+                p = 0;
+            }
+        }
 
+        if (digits[i] !=0 && digits[i] % 10 == 0){
+            p = 1;
+            digits[i] = 0;
+        }
+    }
+    if (p == 1){
+        int[] ints = new int[digits.length + 1];
+        ints[0] = 1;
+        for (int i : digits) {
+            ints[i+1] = digits[i];
+        }
+        return ints;
+    }
+    return digits;
+}
 
-
+//优雅实现
+public int[] plusOne(int[] digits){
+    int len = digits.length;
+    for(int i = len -1; i >=0 ; i--){
+        digits[i] += 1;
+        digits[i] %= 10;
+        if (digits[i] != 0) {
+            return digits;
+        }
+    }
+    digits = new int[len + 1];
+    digits[0] = 1;
+    return digits;
+}
 
 
 
