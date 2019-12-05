c68e7b89e990dc030e34816a082dc0585c575360
Kai Hu <hukai@sunlands.com>
2019-12-05 19:57:23 +0800

    first:70

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -576,13 +576,46 @@ public String addBinary(String a, String b){
 */
 //二分法
 public int mySqrt(int x) {
- 
+
 }
 
 /**
-70.
-*/
+70.爬楼梯
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+注意：给定 n 是一个正整数。
 
+示例 1：
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+示例 2：
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+*/
+//找规律，斐波那契数列，递归，O(n^2)
+public static int climbStairs(int n){
+    return n <= 3 ? n : climbStairs(n -1) + climbStairs( n -2);
+}
+
+//类似斐波那契数列 f(n) = f(n-1)+f(n-2) {n > 3}
+public static int climbStairs2(int n){
+    if (n <= 3) return n;
+    int first = 2, second = 3;
+    int result = 0;
+    for (int i = 3; i < n; i++) {
+        result = first + second;
+        first = second;
+        second = result;
+    }
+    return result;
+}
 
 
 
