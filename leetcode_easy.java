42400774cc01b7760237645cc0efa45890ad01e4
Kai Hu <hukai@sunlands.com>
2019-12-09 20:02:59 +0800

    first:101

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -725,15 +725,47 @@ public boolean isSameTree(TreeNode p, TreeNode q) {
     if (p == null && q == null){
         return true;
     }
-    
-    if (p != null && q != null) {
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    if (p == null || q == null) {
+        return false;
     }
-    return false;
+    
+    if (p.val !=  q.val) {
+        return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
 
+/**
+101.对称二叉树
+给定一个二叉树，检查它是否是镜像对称的。
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+说明:
+如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+*/
+//递归
+public boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    return isMirror(root.left, root.right);
+}
 
-
+public boolean isMirror(TreeNode t1, TreeNode t2){
+    if (t1 == null && t2 == null) return true;
+    if (t1 == null || t2 == null) return false;
+    if (t1.val != t2.val) return false;
+    return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+}
 
 
 
