c23c8a229a23444e65161bbfbe776e5d8e38a580
Kai Hu <hukai@sunlands.com>
2019-12-10 16:14:33 +0800

    first:108

--- leetcode_easy.java
+++ leetcode_easy.java
@@ -862,8 +862,37 @@ private void dfs(TreeNode root, int depth, List<List<Integer>> re) {
     dfs(root.right, depth +1, re);
 }
 
-
-
+/**
+108.将有序数组转换成二叉搜索树
+将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+示例:
+给定有序数组: [-10,-3,0,5,9],
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+ //1.递归 2.二分
+public static TreeNode sortedArrayToBST(int[] nums){
+    if (nums == null || nums.length == 0) return null;
+    int length = nums.length;
+    int i = length / 2;
+    TreeNode root = new TreeNode(nums[i]);
+    int[] leftArr = Arrays.copyOfRange(nums, 0, i);
+    int[] rightArr = Arrays.copyOfRange(nums, i+1, length);
+    TreeNode left = sortedArrayToBST(leftArr);
+    TreeNode right = sortedArrayToBST(rightArr);
+    if (left != null){
+        root.left = left;
+    }
+    if (right != null){
+        root.right = right;
+    }
+    return root;
+}
 
 
 
